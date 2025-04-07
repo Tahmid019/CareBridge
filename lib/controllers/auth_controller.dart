1806cac5f931ccfc2fa3716d389407e0a576cd03
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-04-07 18:18:59 +0530

    upd

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -4,29 +4,44 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{
+class AuthController extends GetxController {
   TextEditingController fullNameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  signupUser() async{
+
+  signupUser() async {
+    FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: emailController.text,
+            password: passwordController.text)
+        .then((userCredential) {
+     
+    });
+
     UserCredential? userCredential;
-    userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
-    if(userCredential != null){
-      await storeUserData(userCredential.user!.uid, fullNameController.text, emailController.text);
+    if (userCredential != null) {
+      await storeUserData(userCredential.user!.uid,
+          fullNameController.text, emailController.text);
     }
   }
 
-  storeUserData(String uid, String fullName, String email) async{
+  storeUserData(String uid, String fullName, String email) async {
     var storeUser = FirebaseFirestore.instance.collection('users').doc(uid);
-    await storeUser.set({
-      'fullName' : fullName,
-      'email' : email
+    await storeUser.set({'fullName': fullName, 'email': email});
+  }
+
+  login(BuildContext context) async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+            email: emailController.text,
+            password: passwordController.text)
+        .then((value) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => HomePageScreen()));
     });
   }
-  login(BuildContext context) async{
-    await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text).then((value) {Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePageScreen()));});
-  }
-  signout() async{
+
+  signout() async {
     await FirebaseAuth.instance.signOut();
   }
 }
